e8de0d7735d841300d1f988df6cd66bad00c8542
Juan Cuzmar <jcuzmar@protonmail.com>
2021-04-14 15:07:37 -0400

    Test more clean code

---
+++ backend/src/test/groovy/me/jics/MockPharmacyController.groovy
@@ -0,0 +1,34 @@
+package me.jics
+
+import io.micronaut.context.annotation.Requires
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.reactivex.Flowable
+
+import java.time.LocalDate
+import java.time.LocalTime
+
+@Requires(property = 'spec.name', value = 'mockPharmacy')
+@Controller('/pharmacies')
+class MockPharmacyController {
+    @Get
+    Flowable<Pharmacy> index() {
+        return Flowable.just(Pharmacy.builder()
+                .date(LocalDate.now())
+                .storeId('1')
+                .storeName('store')
+                .boroughName('borough')
+                .locationName('location')
+                .storeAddress('address')
+                .openingHourOperation(LocalTime.now())
+                .closingHourOperation(LocalTime.now())
+                .storePhone('+56999999999')
+                .storeLat(0.0)
+                .storeLng(0.0)
+                .openingHourOperation(LocalTime.now())
+                .regionFk(1)
+                .boroughFk(1)
+                .build())
+    }
+
+}
--- backend/src/test/groovy/me/jics/PharmacyClientSpec.groovy
+++ backend/src/test/groovy/me/jics/PharmacyClientSpec.groovy
@@ -1,33 +1,17 @@
 package me.jics
 
 import io.micronaut.context.ApplicationContext
-import io.micronaut.context.annotation.Requires
-import io.micronaut.core.io.socket.SocketUtils
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
 import io.micronaut.runtime.server.EmbeddedServer
-import io.reactivex.Flowable
 import spock.lang.AutoCleanup
 import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Subject
 
-import java.time.LocalDate
-import java.time.LocalTime
-
 class PharmacyClientSpec extends Specification {
-
-    @Shared
-    int port = SocketUtils.findAvailableTcpPort()
 
     @AutoCleanup
     @Shared
-    EmbeddedServer server = ApplicationContext.run(EmbeddedServer, [
-            'services.minsal.url'  : "http://localhost:$port",
-            'services.minsal.path' : '/pharmacies',
-            'spec.name'            : 'mockPharmacy',
-            'micronaut.server.port': port
-    ]) as EmbeddedServer
+    EmbeddedServer server = ApplicationContext.run(EmbeddedServer) as EmbeddedServer
 
     @Shared
     @Subject
@@ -39,30 +23,4 @@ class PharmacyClientSpec extends Specification {
         then:
         pharmacy.getStoreId() == '1'
     }
-
-
-    @Requires(property = 'spec.name', value = 'mockPharmacy')
-    @Controller("/pharmacies")
-    static class MockPharmacy {
-        @Get
-        Flowable<Pharmacy> index() {
-            Flowable.just(Pharmacy.builder()
-                    .date(LocalDate.now())
-                    .storeId("1")
-                    .storeName("store")
-                    .boroughName("borough")
-                    .locationName("location")
-                    .storeAddress("address")
-                    .openingHourOperation(LocalTime.now())
-                    .closingHourOperation(LocalTime.now())
-                    .storePhone("+56999999999")
-                    .storeLat(0.0)
-                    .storeLng(0.0)
-                    .openingHourOperation(LocalTime.now())
-                    .regionFk(1)
-                    .boroughFk(1)
-                    .build())
-        }
-    }
-
 }
