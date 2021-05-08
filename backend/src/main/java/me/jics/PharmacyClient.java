441b2b9688625b1cc2c5ef4d606d41cfa914beb4
Juan Cuzmar <jcuzmar@protonmail.com>
2021-05-08 01:30:53 -0400

    Retryable update

--- backend/src/main/java/me/jics/PharmacyClient.java
+++ backend/src/main/java/me/jics/PharmacyClient.java
@@ -1,7 +1,13 @@
 package me.jics;
 
+import io.micronaut.http.annotation.Get;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.retry.annotation.Recoverable;
+import io.reactivex.Flowable;
 
 @Client("${services.minsal.url}")
+@Recoverable(api = PharmacyOperations.class)
 interface PharmacyClient extends PharmacyOperations {
+    @Get(value = "${services.minsal.path}?${services.minsal.param}=${services.minsal.value}")
+    Flowable<Pharmacy> retrieve();
 }
